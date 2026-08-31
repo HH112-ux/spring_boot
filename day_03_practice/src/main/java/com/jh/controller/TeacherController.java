80ec9fe227c44fb90c4098f5774f45e0b914e506
HH112-ux <1986132486@qq.com>
2026-08-31 15:24:16 +0800

    springboot代码学习

--- day_03_practice/src/main/java/com/jh/controller/TeacherController.java
+++ day_03_practice/src/main/java/com/jh/controller/TeacherController.java
@@ -2,6 +2,8 @@ package com.jh.controller;
 
 import com.jh.model.Student;
 import com.jh.model.Teacher;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/teacher")
+@Api(tags = "教师信息管理")
 public class TeacherController {
 
     @Autowired
     private Teacher teacher;
 
     @GetMapping("/info")
+    @ApiOperation("查询教师信息")
     public String teacherInfo() {
         return "教师姓名：" + teacher.getName() +
                 "，性别：" + teacher.getGender() +
@@ -24,11 +28,13 @@ public class TeacherController {
     }
 
     @GetMapping("/students")
+    @ApiOperation("查询所有学生")
     public List<Student> students() {
         return teacher.getStudents();
     }
 
     @GetMapping("/grades")
+    @ApiOperation("查询所有学生成绩")
     public String grades() {
         StringBuilder sb = new StringBuilder();
         for (Student s : teacher.getStudents()) {
@@ -42,6 +48,7 @@ public class TeacherController {
     }
 
     @GetMapping("/chinese")
+    @ApiOperation("查询语文成绩")
     public String chinese() {
         StringBuilder sb = new StringBuilder("语文成绩：\n");
         for (Student s : teacher.getStudents()) {
